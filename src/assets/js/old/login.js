f127535b3c9bf69994060e15d3b648810df3c74a
COULOMB LEO p1925429 <p1925429@univ-lyon1.fr>
2019-11-04 08:14:13 +0100

    Component init

---
+++ src/assets/js/old/login.js
@@ -0,0 +1,120 @@
+$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function(){
+    particlesJS('particles-js',
+      {
+        "particles": {
+          "number": {
+            "value": 100, // Nombre de balle au debut. DEFAULT : 80
+            "density": {
+              "enable": true,
+              "value_area": 800 // JSP mais a 80 c'est très drole. DEFAULT : 800
+            }
+          },
+          "color": {
+            "value": "#FFD400" // Couleur des boules
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#FFD400"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#FFD400", // Couleur des traits
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": true,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": true,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 125 // La distance de la souris a une boule. DEFAULT : 200
+            },
+            "push": {
+              "particles_nb": 4 // Nb de particule creer au clique. DEFAULT 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true,
+        "config_demo": {
+          "hide_card": false,
+          "background_color": "#ffd400",
+          "background_image": "",
+          "background_position": "50% 50%",
+          "background_repeat": "no-repeat",
+          "background_size": "cover"
+        }
+      }
+    );
+
+});
---
+++ src/assets/js/old/menu.js
@@ -0,0 +1,13 @@
+/* Film au box-office */
+$(function () {
+    $('.slideContainer').slick({
+        //touchMove: false,
+        //swipeToSlide: false,
+        //responsive: true,
+        //variableWidth: true,
+        infinite : true,
+        slidesToShow: 5,
+        slidesToScroll : 5,
+        swipe:false,
+    });
+});
