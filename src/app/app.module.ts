7c5d9c03eb7d8c0331b5555c1f615a3e7d8c74c4
FaresMrg <44775892+FaresMrg@users.noreply.github.com>
2020-02-05 01:42:23 +0100

    Update css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { SmartFilmsComponent } from './smart-films/smart-films.component';
 import { FilmMatchComponent } from './film-match/film-match.component';
 import { FilmCardComponent } from './film-card/film-card.component';
 import { MovieModalComponent } from './movie-modal/movie-modal.component';
+import { ProfilComponent } from './profil/profil.component';
 
 const appRoutes: Routes = [
   { path: '', component: ReceptionComponent },
@@ -29,7 +30,7 @@ const appRoutes: Routes = [
   { path: 'legal-mention', component: LegalMentionComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'smart-movies', component: SmartFilmsComponent },
-  { path: 'modale', component: MovieModalComponent },
+  { path: 'profil', component: ProfilComponent },
 ];
 
 @NgModule({
@@ -48,6 +49,7 @@ const appRoutes: Routes = [
     FilmMatchComponent,
     FilmCardComponent,
     MovieModalComponent,
+    ProfilComponent,
   ],
   imports: [
     BrowserModule,
